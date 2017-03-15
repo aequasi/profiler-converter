613479e46e14ef3713f0b0beed0eb87db3c128a9
Aaron Scherer <aequasi@gmail.com>
2017-03-15 01:49:13 -0700

    Adding fork ribbon

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/', (req, res) => {
         </style>
     </head>
     <body>
+        <a href="https://github.com/aequasi/profiler-converter">
+            <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"><
+        /a>
         <section class="hero is-primary is-fullheight">
             <div class="hero-head">
                 <header class="nav">
